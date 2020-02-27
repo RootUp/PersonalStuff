8bce73f80b9c5705ba227561bc25791201a1e81f
Dhiraj Mishra <mishra.dhiraj95@gmail.com>
2020-02-27 15:25:14 +0400

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -14,7 +14,7 @@ libqt5core5a libqtcore4 libqt5gui5 libqt5network5 libqt4-network libreadline-dev
 lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev fossil libavis-dev harvid libavahi-glib-dev \
 libswscale-dev liba52-0.7.4-dev libxcb-xkb-dev libxcb-composite0-dev libdssialsacompat-dev alsa-utils \
 libao-dev apt-file python-pip dput librivet-dev libcue-dev libbellesip-dev libbctoolbox-dev libantlr3c-dev \
-nvptx-tools texi2html libgnutls-dane0 gnuastro auctex a2ps ; sudo apt-get install --reinstall texinfo ; sudo apt-get install libgd-dev \
+nvptx-tools texi2html libgnutls-dane0 gnuastro auctex a2ps ; sudo apt-get install -y --reinstall texinfo ; sudo apt-get install -y libgd-dev \
 libsynctex-dev unrar php7.1 postgresql-client-10 libqt4-sql-psql pspg libsipwitch-dev sipwitch p7zip-full zlib1g libzzip-dev \
 libsdl2-dev openssh-server libelf-dev zstd libboost-all-dev g++ automake autoconf autoconf-archive libtool liblz4-dev liblzma-dev \
 zlib1g-dev make libjsoncpp-dev libiberty-dev qemu-kvm qemu virt-manager virt-viewer libvirt-bin \
